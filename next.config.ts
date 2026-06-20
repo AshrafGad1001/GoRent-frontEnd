6e5c88dca91c8412dc8c162530f1521abc4aa458
Nourhan Adel <143334844+Nora-Shalaby@users.noreply.github.com>
2026-06-20 12:05:46 +0300

    deploy backend on vercel and connect next app to it

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,21 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
       },
     ],
   },
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        source: "/api/:path*",
+        destination: "https://backend-go-rent.vercel.app/api/:path*",
+        // destination: 'http://localhost:5000/api/:path*',
       },
     ];
   },
